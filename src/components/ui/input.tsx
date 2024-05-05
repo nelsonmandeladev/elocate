92ca013258d52414bf546b61b5c760060b57a8ce
MEASDEV <sonfacknelsonmandela@gmail.com>
2024-05-05 18:39:15 +0100

    Added create location feature

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-0 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 focus:border-spacing-0 focus:border-primary outline-0 focus:outline-0",
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-[16px] ring-offset-0 file:border-0 file:bg-transparent file:text-[16px] file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 focus:border-spacing-0 focus:border-primary outline-0 focus:outline-0",
           className
         )}
         ref={ref}
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[130px] md:min-h-[200px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-transparent placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 focus:border-primary",
+          "flex min-h-[130px] md:min-h-[200px] w-full rounded-md border border-input bg-background px-3 py-2 text-[16px] ring-offset-transparent placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 focus:border-primary",
           className
         )}
         ref={ref}
