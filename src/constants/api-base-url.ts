064f9f113696f73185d62cd8056ba9517e994b33
MEASDEV <sonfacknelsonmandela@gmail.com>
2024-05-10 17:04:54 +0100

    Added share icons

--- src/constants/api-base-url.ts
+++
@@ -1,1 +0,0 @@
-export const BASE_URL: string | undefined = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL : 'http://localhost:3000/api/';
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export * from "./translations-namespaces";
-export * from "./api-base-url";
+export * from "./base-urls";
 export * from "./returnable-fields";
